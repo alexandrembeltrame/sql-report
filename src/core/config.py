4b8c8f0a1c4c0e339b8f53bac63e471ee1945724
alexandrembeltrame <mbalexandre@hotmail.com>
2025-10-30 19:45:44 -0300

    altered bd to postgresl more robust

--- src/core/config.py
+++ src/core/config.py
@@ -1,13 +1,19 @@
-from pydantic import BaseModel
-from dotenv import load_dotenv
-import os
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
-# Carrega as variáveis do .env
-load_dotenv()
+class Settings(BaseSettings):
+    DB_USER: str = "postgres"
+    DB_PASSWORD: str = "postgres"
+    DB_HOST: str = "localhost"
+    DB_PORT: int = 5432
+    DB_NAME: str = "sql_report"
 
-class Settings(BaseModel):
-  DATABASE_URL: str = os.getenv("DATABASE_URL")
-  ENVIRONMENT: str = os.getenv("ENVIRONMENT", "development")
-  DEBUG: bool = os.getenv("DEBUG", "false").lower() == "true"
+    model_config = SettingsConfigDict(env_file=".env", case_sensitive=True)
+
+    @property
+    def DATABASE_URL(self):
+        return (
+            f'postgresql+psycopg2://{self.DB_USER}:{self.DB_PASSWORD}'
+            f'@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}'
+        )
 
 settings = Settings()
--- src/models/employee_model.py
+++ src/models/employee_model.py
@@ -5,7 +5,7 @@ class Employee(Base):
   __tablename__ = "employees"
 
   id = Column(Integer, primary_key=True, index=True)
-  name = Column(String, nullable=False)
-  department = Column(String, nullable=False)
+  name = Column(String(100), nullable=False)
+  department = Column(String(50), nullable=False)
   salary = Column(Float, nullable=False)
   performance = Column(Integer, nullable=False)
